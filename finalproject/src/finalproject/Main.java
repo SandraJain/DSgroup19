c2235175c5e6bec5669e0152656cc784eca913fd
ivy926 <74865268+ivy926@users.noreply.github.com>
2021-12-27 14:59:49 +0800

    12/27 update

--- finalproject/src/finalproject/Main.java
+++ finalproject/src/finalproject/Main.java
@@ -6,34 +6,15 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-		System.out.println("Please type: Keyword");
-		//HW10
-		Scanner sc = new Scanner(System.in);
-		while(sc.hasNextLine()){
-		    //String urlStr = sc.next();
-		    String keyword = sc.next();
-		    
-		    GoogleQuery googleQuery = new GoogleQuery(keyword);
-		   
-		    //WordCounter counter = new WordCounter(urlStr);
-		    //System.out.println(counter.countKeyword(keyword));
-		    
-		    
-		    WebPageList webPageList = new WebPageList();
-		    WebPage rootPage = new WebPage("http://soslab.nccu.edu.tw/Welcome.html", "Soslab");	//Call Google�����s��
-		    webPageList.add(rootPage);
-			WebTree tree = new WebTree(rootPage);
-			//build childnode
-			tree.root.addChild(new WebNode(new WebPage("http://soslab.nccu.edu.tw/Publications.html","Publication")));//�l�������s��
-			tree.root.addChild(new WebNode(new WebPage("http://soslab.nccu.edu.tw/Projects.html","Projects")));//�l�������s��
-			//tree.root.children.get(1).addChild(new WebNode(new WebPage("https://vlab.cs.ucsb.edu/stranger/", "Stranger")));
-			tree.root.addChild(new WebNode(new WebPage("http://soslab.nccu.edu.tw/Members.html", "MEMBER")));//�l�������s��
-			tree.root.addChild(new WebNode(new WebPage("http://www3.nccu.edu.tw/~yuf/course.htm","Course")));//�l�������s��
-			//read 2 Yu 1.2 Fang 1.8 
-			//Scanner scanner = new Scanner(System.in);
-			
-			
-		      
+		GoogleQuery googleQuery = new GoogleQuery("NCCU");
+		WebPageList webPageList = new WebPageList();
+		WebPage rootPage = new WebPage(googleQuery.url, "Google result");
+		webPageList.add(rootPage);
+		WebTree tree = new WebTree(rootPage);
+		//build childnode
+		googleQuery.query();
+		for(int i = 0; i<20; i++) {
+			tree.root.addChild(new WebNode(new WebPage(googleQuery.citeUrls.get(i), googleQuery.titles.get(i))));
 		}
 	}
 }
