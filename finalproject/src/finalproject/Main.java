d339fc76ba4686f6362ab5fee52aa251972e406a
SandraJain <74034655+SandraJain@users.noreply.github.com>
2022-01-02 17:14:16 +0800

    1/2

--- finalproject/src/finalproject/Main.java
+++ finalproject/src/finalproject/Main.java
@@ -6,15 +6,20 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-		GoogleQuery googleQuery = new GoogleQuery("NCCU");
+		GoogleQuery googleQuery = new GoogleQuery("School"+"+NCCU");
 		WebPageList webPageList = new WebPageList();
 		WebPage rootPage = new WebPage(googleQuery.url, "Google result");
 		webPageList.add(rootPage);
 		WebTree tree = new WebTree(rootPage);
 		//build childnode
-		googleQuery.query();
+		googleQuery.query();//?
 		for(int i = 0; i<20; i++) {
 			tree.root.addChild(new WebNode(new WebPage(googleQuery.citeUrls.get(i), googleQuery.titles.get(i))));
 		}
+		KeywordList keywordList = new KeywordList();
+		
+		tree.setPostOrderScore(keywordList);
+		//QuickSort quicksort =new QuickSort(webPageList);
+		//quicksort.sort();
 	}
 }
