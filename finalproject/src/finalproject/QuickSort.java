f8d267c2a3ef08107bccf767ed9348a213820301
SandraJain <74034655+SandraJain@users.noreply.github.com>
2022-01-09 16:52:51 +0800

    Update QuickSort.java

--- finalproject/src/finalproject/QuickSort.java
+++ finalproject/src/finalproject/QuickSort.java
@@ -1,21 +1,23 @@
 package finalproject;
 
+import java.util.ArrayList;
+
 public class QuickSort {
-	WebPageList list = new WebPageList();
+	ArrayList<WebNode> list = new ArrayList<WebNode>();//WebPageList list = new WebPageList();
 	
-	public QuickSort(WebPageList list){
+	public QuickSort(ArrayList<WebNode> list){
 		this.list=list;
 	}
 	
 	//quick sort
 		public void sort(){
-			if(list.webPageList.size() == 0)
+			if(list.size() == 0)
 			{
 				System.out.println("InvalidOperation");
 			}
 			else 
 			{
-				quickSort(0, list.webPageList.size()-1);
+				quickSort(0, list.size()-1);
 //				System.out.println("Done");
 			}
 
@@ -26,14 +28,14 @@ public class QuickSort {
 			//1. implement quickSort algorithm
 			if(leftbound < rightbound) { //�p�ƨ�j �n�令�j��p
 				int i=(leftbound-1);
-				int pivot=list.webPageList.get(rightbound).score;
+				int pivot=list.get(rightbound).nodeScore; 
 				for(int j=leftbound; j<=rightbound-1;j++) {
-					if(list.webPageList.get(j).score>pivot){
+					if(list.get(j).nodeScore>pivot){
 						i++;
 						swap(i,j);
 					}
 					
-				}
+				} 
 				swap(i+1,rightbound);
 				quickSort(leftbound,i);
 				quickSort(i+2,rightbound);
@@ -42,17 +44,17 @@ public class QuickSort {
 			}
 		
 		private void swap(int aIndex, int bIndex){
-			WebPage temp = list.webPageList.get(aIndex);
-			list.webPageList.set(aIndex, list.webPageList.get(bIndex));
-			list.webPageList.set(bIndex, temp);
+			WebNode temp = list.get(aIndex);
+			list.set(aIndex, list.get(bIndex));
+			list.set(bIndex, temp);
 		}
 		
 		public void output(){
 			//TODO: write output and remove all element logic here...
 			StringBuilder sb = new StringBuilder();
 			
-			for(int i=0; i<list.webPageList.size();i++){
-				WebPage k = list.webPageList.get(i);
+			for(int i=0; i<list.size();i++){
+				WebNode k = list.get(i);
 				if(i>0)sb.append(" ");
 				sb.append(k.toString());
 			}
