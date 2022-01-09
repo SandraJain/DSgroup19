847d9b76e37c89e7b2652eb60d39d9abf8bacaed
SandraJain <74034655+SandraJain@users.noreply.github.com>
2022-01-09 16:55:48 +0800

    Update WebTree.java

--- finalproject/src/finalproject/WebTree.java
+++ finalproject/src/finalproject/WebTree.java
@@ -1,8 +1,7 @@
 package finalproject;
 
-
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.ArrayList; 
 
 
 public class WebTree {
@@ -12,11 +11,11 @@ public class WebTree {
 		this.root = new WebNode(rootPage);
 	}
 	
-	public void setPostOrderScore(KeywordList keywords) throws IOException{
+	public void setPostOrderScore(ArrayList<Keyword> keywords) throws IOException{
 		setPostOrderScore(root, keywords);
 	}
 	
-	private void setPostOrderScore(WebNode startNode, KeywordList keywords) throws IOException{
+	private void setPostOrderScore(WebNode startNode, ArrayList<Keyword> keywords) throws IOException{
 		//1.compute the score of children nodes postorder
 		for(WebNode child : startNode.children){
 			setPostOrderScore(child,keywords);
@@ -69,3 +68,95 @@ public class WebTree {
 		return retVal;
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import java.io.IOException;
+import java.util.ArrayList;
+
+
+public class WebTree {
+	public WebNode root;
+	
+	public WebTree(WebPage rootPage){
+		this.root = new WebNode(rootPage); 
+	}
+	
+	public void setPostOrderScore(ArrayList<Keyword> keywords) throws IOException{//KeywordList
+		setPostOrderScore(root, keywords);
+	}
+	
+	private void setPostOrderScore(WebNode startNode, ArrayList<Keyword> keywords) throws IOException{//KeywordList
+		//1.compute the score of children nodes postorder
+		for(WebNode child : startNode.children){
+			setPostOrderScore(child,keywords);
+		}
+		//**setNode score of startNode
+		startNode.setNodeScore(keywords);
+		}
+	
+	public void eularPrintTree(){
+		eularPrintTree(root);
+	}
+	
+	private void eularPrintTree(WebNode startNode){
+		int nodeDepth = startNode.getDepth();
+		
+		if(nodeDepth > 1) System.out.print("\n" + repeat("\t", nodeDepth-1));
+		//print "("
+		System.out.print("(");
+		//print "name","score"
+		System.out.print(startNode.webPage.name+","+startNode.nodeScore);
+		
+		//2.print child preorder
+		for(WebNode child : startNode.children){
+			eularPrintTree(child);
+			
+		}
+		
+		//print ")"
+		System.out.print(")");
+		
+		/*for example
+		(Soslab,459.0
+				(Publication,286.2)
+				(Projects,42.0
+						(Stranger,0.0)
+				)
+				(MEMBER,12.0)
+				(Course,5.3999999999999995)
+		)
+		*/
+/*
+		if(startNode.isTheLastChild()) System.out.print("\n" + repeat("\t", nodeDepth-2));
+		
+	}
+	
+	private String repeat(String str,int repeat){
+		String retVal  = "";
+		for(int i=0;i<repeat;i++){
+			retVal+=str;
+		}
+		return retVal;
+	}
+}
+*/
+
+
+
